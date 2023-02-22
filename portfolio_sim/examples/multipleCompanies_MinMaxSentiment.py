8f6bdd9d7e887cc01c2a8308b6aae21d5af78376
Martin Mörsell <martin.morsell@northvolt.com>
2023-02-22 10:47:36 +0100

    added examples in python/jupyter notebook format

---
+++ portfolio_sim/examples/multipleCompanies_MinMaxSentiment.py
@@ -0,0 +1,31 @@
+import requests
+import pandas as pd
+
+from portfolio_sim.utils import simulate_stock, load_index_data
+from portfolio_sim.portfolio_simulator import PortfolioSimulator
+from portfolio_sim.strategies.single_strategies import MinMaxSentimentStrategy
+
+if __name__ == "__main__":
+    # Load Company list
+    url = (
+        "https://api.finsentim.com/latest/companies/"
+        "table/?key=jhcfkw0dfqe0gh8zw2eaun82yxggpevd%20"
+    )
+    comp_table = requests.get(url)
+    comp_table = pd.read_json(comp_table.json()).name
+
+    # Load Index Data
+    index_data = load_index_data("2022-08-01", "2022-08-12")
+    index_data = index_data.drop(columns=["MOOD", "BUZZ"])
+
+    # Simulate Portfolio
+    company_data_dict = simulate_stock(comp_table, "2022-08-10", "2022-08-12")
+
+    ps = PortfolioSimulator(
+        company_data_dict,
+        comp_table,
+        MinMaxSentimentStrategy(),
+        index_data,
+    )
+    ps.simulate_portfolio()
+    print(ps.compare_baselines())
